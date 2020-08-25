8f6f51cad95650619b38adfbe65abe434acdfd3f
mmmzcl <1207919323@qq.com>
2020-08-24 23:34:55 -0700

    redirect_index

--- index.py
+++ index.py
@@ -1,5 +1,8 @@
 from django.http import HttpResponse
-
+from django.shortcuts import redirect
 
 def index(request):
     return  HttpResponse("index")
+
+def redirect(request):
+    return redirect('/index')
